7372c16ecf5a88bc5ff7ce611eaf146f2d531e4e
codejedi365 <codejedi365@gmail.com>
2022-10-24 20:25:17 -0400

    ci(semantic-release): set additional commit types for version detection

--- .releaserc.js
+++ .releaserc.js
@@ -1,9 +1,28 @@
 module.exports = {
-    branches: ["master", "next"],
+    branches: [ "master", "next" ],
     plugins: [
-        "@semantic-release/commit-analyzer",
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "angular",
+                releaseRules: [
+                    { type: "build", scope: "deps", release: "patch" },
+                    { type: "build", scope: "deps-peer", release: "patch" },
+                    { type: "build", scope: "deps-dev", release: false },
+                    { type: "docs", scope: "README", release: "patch" },
+                    { type: "docs", scope: "LICENSE", release: "patch" }
+                    // Continue matching via commit-analyzer/lib/default-release-rules.js
+                    // which are the following for the angular preset:
+                    // { breaking: true, release: 'major' },
+                    // { revert: true, release: 'patch' },
+                    // { type: 'feat', release: 'minor' },
+                    // { type: 'fix', release: 'patch' },
+                    // { type: 'perf', release: 'patch' },
+                ]
+            }
+        ],
         "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
+        // "@semantic-release/changelog",
         "@semantic-release/npm",
         "@semantic-release/github",
         [
